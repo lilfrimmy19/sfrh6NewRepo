593c6f9095f60b320b196672702ac3a4d1e0e35e
samuelfrimpongjr <sfrh6@mail.missouri.edu>
2019-09-26 11:01:59 -0500

    removed unneeded test files

--- pytesting/tests/test_phonenumber.py
+++
@@ -1,5 +0,0 @@
-import phonenumber
-
-#checking the validity of a phonenumber
-def test_validtelephone():
-	assert phonenumber.validtelephone("563-859-3832") == True
--- pytesting/tests/test_yield_generator.py
+++
@@ -1,6 +0,0 @@
-import yield_generator
-
-# testing to see of a prime number has been generated or not
-def test_mygenerator():
-	
-	assert yield_generator.checkforprime() == "Found Prime"
